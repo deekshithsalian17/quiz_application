fd846ff8a51287caba8f553ea3bead2a96d79ae8
deekshithsalian17 <deekshithsalian17@gmail.com>
2023-06-05 15:28:23 +0530

    dsds

---
+++ lib/services/question_anwer_list.dart
@@ -0,0 +1,52 @@
+Map<int, String> Questions = {
+  1:'Which programming language is used to build Flutter applications?',
+  2:'What command would you use to compile your Flutter app in release mode?',
+  3:'How many types of widgets are there in Flutter?'
+};
+
+Map<int, String> OptionsForQ1 = {
+  0: '1.  Kotlin',
+  1: '2.  Java',
+  2: '3.  Dart',
+  3:'4.  Go',
+};
+
+
+Map<int, bool> OptionsForQ1Answer = {
+  0: false,
+  1: false,
+  2: true,
+  3: false,
+};
+
+
+Map<int, String> OptionsForQ2 = {
+  0: '1.  Flutter run --release',
+  1: '2.  Flutter --release',
+  2: '3.  Flutter build --release',
+  3: '4.  Flutter run release',
+};
+
+
+Map<int, bool> OptionsForQ2Answer = {
+  0: true,
+  1: false,
+  2: false,
+  3: false,
+};
+
+
+Map<int, String> OptionsForQ3 = {
+  0: '1.  5',
+  1: '2.  3',
+  2: '3.  1',
+  3: '4.  2',
+};
+
+
+Map<int, bool> OptionsForQ3Answer = {
+  0: false,
+  1: false,
+  2: false,
+  3: true,
+};
